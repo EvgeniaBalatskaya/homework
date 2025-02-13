0c487db4aa91f730a7b9e21230f7e77ca8912faf
Evgenia <evgeniabalackaa6@gmail.com>
2025-02-13 19:31:02 +0300

    commit

--- masks.py
+++ masks.py
@@ -4,7 +4,7 @@ def get_mask_card_number(card_number: int) -> str:
     return f"{card_str[:4]} {card_str[4:6]}** **** {card_str[-4:]}"
 
 def get_mask_account(account_number: int) -> str:
-    """Возвращает маску номера счета в формате **XXXX."""
+    """Возвращает маску номера счета в формате **XXXX. """
     account_str = str(account_number)
     return f"**{account_str[-4:]}"  # Показываем только последние 4 цифры
 
