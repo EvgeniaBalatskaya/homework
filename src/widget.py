ce4d05082b925451e55e4c4fadffee6a8a1272d7
Evgenia <evgeniabalackaa6@gmail.com>
2025-02-20 16:47:48 +0300

    Fix

--- src/widget.py
+++ src/widget.py
@@ -26,4 +26,3 @@ def get_date(date_str: str) -> str:
     Форматирует дату из '2024-03-11T02:26:18.671407' в '11.03.2024'.
     """
     return datetime.fromisoformat(date_str).strftime("%d.%m.%Y")
-    
